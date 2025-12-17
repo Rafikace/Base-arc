35d17d4b7fd53d4d64a840f810bfad9780625066
Biokes <106444765+Biokes@users.noreply.github.com>
2025-12-17 18:54:34 +0100

    update(integration): added squareClickHandler

--- frontend/components/chess/index.tsx
+++ frontend/components/chess/index.tsx
@@ -4,8 +4,25 @@ import { initializeBoard } from "@/lib/utils";
 import { useState } from "react";
 
 export default function Chess() {
+
     const [board] = useState(initializeBoard());
     const [selectedSquare, setSelectedSquare] = useState(null);
+
+    const handleSquareClick = (row, col) => {
+        const piece = board[row][col];
+
+        if (selectedSquare) {
+            const [fromRow, fromCol] = selectedSquare;
+            handleMakeMove(fromRow, fromCol, row, col);
+
+            setSelectedSquare(null);
+        } else {
+            if (piece) {
+                setSelectedSquare([row, col]);
+            }
+        }
+    };
+
     return (
         <>
         </>
