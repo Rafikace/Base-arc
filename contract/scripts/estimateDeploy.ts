1fdf4c22672ddf9ef88efc3f83b4df744c0bf2cb
Biokes <106444765+Biokes@users.noreply.github.com>
2025-12-18 00:19:23 +0100

    update(integration): deploy contract and verify pingpong game contract

--- contract/scripts/estimateDeploy.ts
+++ contract/scripts/estimateDeploy.ts
@@ -1,33 +1,3 @@
-// import hre from "hardhat";
-
-
-// const ethers = await hre.network.connect();
-
-// async function main() {
-//   const factory = await ethers.getContractFactory("PingPong");
-
-//   const deployTx = await factory.getDeployTransaction();
-
-//   const gasUsed = await ethers.provider.estimateGas(deployTx);
-
-//   const feeData = await ethers.provider.getFeeData();
-
-//   const gasPrice =
-//     feeData.maxFeePerGas ?? feeData.gasPrice!;
-
-//   const costWei = gasUsed * gasPrice;
-
-//   console.log("Gas estimate:", gasUsed.toString());
-//   console.log("Gas price (wei):", gasPrice.toString());
-//   console.log("Estimated deploy cost (wei):", costWei.toString());
-//   console.log(
-//     "Estimated deploy cost (ETH):",
-//     ethers.formatEther(costWei)
-//   );
-// }
-
-// main();
-
 import hre from "hardhat";
 
 async function main() {
