12b2c8e7fe76533ea7c66cf0377e1603c674db15
Biokes <106444765+Biokes@users.noreply.github.com>
2025-12-17 18:53:03 +0100

    update(integration): added state managements for chess

--- frontend/components/chess/index.tsx
+++ frontend/components/chess/index.tsx
@@ -1,7 +1,13 @@
+"use client";
+
+import { initializeBoard } from "@/lib/utils";
+import { useState } from "react";
+
 export default function Chess() {
+    const [board] = useState(initializeBoard());
+    const [selectedSquare, setSelectedSquare] = useState(null);
     return (
         <>
         </>
     )
 }
- 
--- frontend/lib/utils.ts
+++ frontend/lib/utils.ts
@@ -1,56 +1,65 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
-import { cookieStorage, createStorage } from '@wagmi/core'
-import { WagmiAdapter } from '@reown/appkit-adapter-wagmi'
-import { baseSepolia, base } from '@reown/appkit/networks'
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+import { cookieStorage, createStorage } from "@wagmi/core";
+import { WagmiAdapter } from "@reown/appkit-adapter-wagmi";
+import { baseSepolia, base } from "@reown/appkit/networks";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+	return twMerge(clsx(inputs));
 }
 
 export const wagmiAdapter = new WagmiAdapter({
-  storage: createStorage({
-    storage: cookieStorage
-  }),
-  ssr: true,
-  projectId: process.env.NEXT_PUBLIC_PROJECT_ID!,
-  networks: [base, baseSepolia]
-})
+	storage: createStorage({
+		storage: cookieStorage
+	}),
+	ssr: true,
+	projectId: process.env.NEXT_PUBLIC_PROJECT_ID!,
+	networks: [base, baseSepolia]
+});
 
 export const config = wagmiAdapter.wagmiConfig;
 export const pieceSymbols = {
-  white: {
-    king: '♔',
-    queen: '♕',
-    rook: '♖',
-    bishop: '♗',
-    knight: '♘',
-    pawn: '♙',
-  },
-  black: {
-    king: '♚',
-    queen: '♛',
-    rook: '♜',
-    bishop: '♝',
-    knight: '♞',
-    pawn: '♟',
-  },
-};  
+	white: {
+		king: "♔",
+		queen: "♕",
+		rook: "♖",
+		bishop: "♗",
+		knight: "♘",
+		pawn: "♙"
+	},
+	black: {
+		king: "♚",
+		queen: "♛",
+		rook: "♜",
+		bishop: "♝",
+		knight: "♞",
+		pawn: "♟"
+	}
+};
 export const initializeBoard = () => {
-  const board = Array(8)
-    .fill(null)
-    .map(() => Array(8).fill(null));
+  const board: {type: string, color:string}[][] = Array(8)
+		.fill(null)
+		.map(() => Array(8).fill(null));
 
-  for (let i = 0; i < 8; i++) {
-    board[1][i] = { type: 'pawn', color: 'black' };
-    board[6][i] = { type: 'pawn', color: 'white' };
-  }
+	for (let i = 0; i < 8; i++) {
+		board[1][i] = { type: "pawn", color: "black" };
+		board[6][i] = { type: "pawn", color: "white" };
+	}
 
-  const setup = ['rook', 'knight', 'bishop', 'queen', 'king', 'bishop', 'knight', 'rook'];
-  for (let i = 0; i < 8; i++) {
-    board[0][i] = { type: setup[i], color: 'black' };
-    board[7][i] = { type: setup[i], color: 'white' };
-  }
+	const setup = [
+		"rook",
+		"knight",
+		"bishop",
+		"queen",
+		"king",
+		"bishop",
+		"knight",
+		"rook"
+	];
+	for (let i = 0; i < 8; i++) {
+		board[0][i] = { type: setup[i], color: "black" };
+		board[7][i] = { type: setup[i], color: "white" };
+	}
 
-  return board;
+	return board;
 };
